296e693eca10ce96ca92dbb4628898ce0fc96f59
Dev00kushal <devkushal00@gmail.com>
2023-11-23 19:09:16 +0545

    ✅  Update code to add, update, and delete todo item

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,15 +2,43 @@ import React, { useState } from "react";
 
 const App = () => {
   const [todo, setTodo] = useState([]);
+  const [id, setId] = useState(1);
   const [input, setInput] = useState("");
-
+  const [todoState, setTodoState] = useState("Add");
   const addTodo = () => {
-    setTodo([...todo, input.trim()]);
-    setInput("");
+    if (input.length !== 0) {
+      setTodo([
+        ...todo,
+        {
+          id,
+          todo: input.trim(),
+        },
+      ]);
+      setId(id + 1);
+      console.log(id);
+      setInput("");
+    }
+  };
+  const deleteButton = (index) => {
+    setTodo(
+      todo?.filter((_, idx) => {
+        return idx !== index;
+      })
+    );
+  };
+
+  const updateValueInput = (index) => {
+    setInput(todo[index].todo);
+    setTodoState("Update");
+  };
+
+  const updateValue = (index) => {
+    setTodo([...todo, input]);
   };
 
   return (
     <div className="h-screen flex flex-1 justify-center items-center space-x-3">
+      <h1>{id}</h1>
       <input
         type="text"
         placeholder="Type here..."
@@ -18,15 +46,33 @@ const App = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <button className="btn btn-danger" onClick={addTodo}>
-        Add
-      </button>
+      {todoState == "Add" ? (
+        <button className="btn btn-danger" onClick={addTodo}>
+          Add
+        </button>
+      ) : (
+        <button className="btn btn-danger" onClick={() => updateValue}>
+          Update
+        </button>
+      )}
 
-      <div className="bg-red-400 text-white">
+      <div className=" text-white ">
         {todo?.map((val, index) => {
           return (
             <div key={index}>
-              <li>{val}</li>
+              <li>{val.todo}</li>
+              <button
+                className="btn btn-error btn-sm"
+                onClick={() => deleteButton(index)}
+              >
+                Delete
+              </button>
+              <button
+                className="btn btn-primary btn-sm ml-5"
+                onClick={() => updateValueInput(index)}
+              >
+                Update
+              </button>
             </div>
           );
         })}
